ce724c0d91057085d9b29200fa18de47e2dd7a7f
Charlene <charlenechetcuti@gmail.com>
2018-06-01 14:19:51 +0100

    describe spec

--- spec/describeSpec.js
+++ spec/describeSpec.js
@@ -1,11 +1,22 @@
 const description = 'test description'
-let assertions = ['example assertion'];
+let assertions = [];
 let callbackCalls = 0;
+
+let tests = [];
 
 function callback() {
   callbackCalls += 1;
 }
 
-describe(description, callback);
+function AssertionGroup(description, assertions){
+  this.name = "fakegroup"
+
+}
+
+
+const result = describe(description, callback, AssertionGroup, tests);
 
 expect(callbackCalls).toEqual(1);
+expect(tests[0]).toEqual(new AssertionGroup(description, assertions))
+expect(tests.length).toEqual(1)
+expect(result).toEqual(tests[0])
